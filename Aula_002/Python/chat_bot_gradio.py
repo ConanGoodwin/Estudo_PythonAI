5a235061484702eb043fe6c6696891564a59ecde
ConanGoodwin <conan_goodwin@hotmail.com>
2024-11-08 20:26:55 -0300

    feat: Aula_002 add um .py de um chat bot que le arquivos e que alem de passar um contexto inicial, trata erros de arquivos ainda em processamento e arquivos em formatos que o gemini não entende, guiando o chat bot a retornar uma menssagem mais amigavel do que um codigo de erro.

--- Aula_002/Python/chat_bot_gradio.py
+++ Aula_002/Python/chat_bot_gradio.py
@@ -3,7 +3,8 @@ import gradio as gr
 import os
 
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
-model = genai.GenerativeModel('gemini-1.5-flash')
+initial_prompt = "Você é um professor de programação e de desenvolvimeto de software, fale como um professor."
+model = genai.GenerativeModel('gemini-1.5-flash', system_instruction=initial_prompt)
 
 chat = model.start_chat()
 
