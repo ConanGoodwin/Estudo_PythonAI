a27713f041f115e1b90a1368bf6df5b9cf191976
ConanGoodwin <conan_goodwin@hotmail.com>
2024-11-10 14:48:35 -0300

    feat: base of Flask

--- Aula_004/Python/app.py
+++ Aula_004/Python/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,6 +7,18 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("home.html", nome="Aluno")
+
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+
+@app.route("/feedback", methods=["POST"])
+def feedback():
+    feedback_text = request.form["feedback"]
+
+    return f"Obrigado pelo seu Feedback: {feedback_text}"
 
 
 if __name__ == "__main__":
