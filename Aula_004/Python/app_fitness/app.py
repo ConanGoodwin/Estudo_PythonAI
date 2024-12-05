bc959b91cd625549a9de69aaf80a5be8d659d0bb
ConanGoodwin <conan_goodwin@hotmail.com>
2024-12-05 09:38:15 -0300

    chore: att

--- Aula_004/Python/app_fitness/app.py
+++ Aula_004/Python/app_fitness/app.py
@@ -84,4 +84,7 @@ def user_details(user_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    from waitress import serve
+
+    serve(app, host="0.0.0.0", port=8080)
