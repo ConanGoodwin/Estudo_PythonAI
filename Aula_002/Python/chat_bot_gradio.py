6c52da8c0a8742222c7b160c59713ddc9c18fbb7
ConanGoodwin <conan_goodwin@hotmail.com>
2025-04-19 18:57:27 -0300

    chore: att flint

--- Aula_002/Python/chat_bot_gradio.py
+++ Aula_002/Python/chat_bot_gradio.py
@@ -3,8 +3,10 @@ import gradio as gr
 import os
 
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
-initial_prompt = "Você é um professor de programação e de desenvolvimeto de software, fale como um professor."
-model = genai.GenerativeModel('gemini-1.5-flash', system_instruction=initial_prompt)
+initial_prompt = "Você é um professor de programação e de desenvolvimeto de software, fale como um professor."  # noqa: E501
+model = genai.GenerativeModel(
+    "gemini-1.5-flash", system_instruction=initial_prompt
+)  # noqa: E501
 
 chat = model.start_chat()
 
